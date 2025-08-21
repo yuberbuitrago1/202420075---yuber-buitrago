623f1ded632172929e196fb886022647a1e16e9c
yuberbuitrago1 <yuber.buitrago@uptc.edu.co>
2025-08-21 16:49:33 +0000

    agregar variables

--- HelloWorld/helloworld3.cpp
+++ HelloWorld/helloworld3.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
+    string userName = "yuber";
+    int CodeStudent = 202420075;
     cout << "HelloWorld" << endl;
-    cout << "soy yuber" << endl;
-    cout << "codigo: 202420075 de ingenieria electronica" <<endl;
+    cout << "soy " << userName << endl;
+    cout << "codigo: " << CodeStudent <<" de ingenieria electronica" <<endl;
 
     return 0;
 }
