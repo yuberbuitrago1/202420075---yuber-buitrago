9c29737633411f625431f7df613856f65053e18a
yuberbuitrago1 <yuber.buitrago@uptc.edu.co>
2025-08-21 16:29:39 +0000

    se inserto otra linea de codigo

--- HelloWorld/helloworld3.cpp
+++ HelloWorld/helloworld3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     cout << "HelloWorld" << endl;
     cout << "soy yuber" << endl;
+    cout << "codigo: 202420075 de ingenieria electronica" <<endl;
 
     return 0;
 }
